c995d38a77684a39e084025eda3099cd8f517fd7
amreshk005 <amreshk005@gmail.com>
2019-08-30 23:59:11 +0530

    auth added

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Switch, Redirect} from "react-router-dom";
 
 import { Provider as AlertProdvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
@@ -7,6 +8,9 @@ import AlertTemplate from 'react-alert-template-basic';
 import Header from './layout/Header';
 import Dashboard from './leads/Dashboard';
 import Alerts from './layout/Alerts';
+import Register from './accounts/Register';
+import Login from './accounts/Login';
+
  
 import { Provider } from 'react-redux';
 import store from '../store';
@@ -22,13 +26,20 @@ class App extends Component {
         return (
             <Provider store={store}>
             <AlertProdvider template={AlertTemplate} {...alertOptions}>
+            <Router>
             <Fragment>
             <Header />
             <Alerts />
             <div className="container">
+            <Switch>
+                <Route exact path="/" Component={Dashboard} />
+                <Route exact path="/register" Component={Register} />
+                <Route exact path="/login" Component={Login} />
+            </Switch> 
             <Dashboard />
             </div>
             </Fragment>
+            </Router>
             </AlertProdvider>
             </Provider>
         )
--- leadmanager/frontend/src/components/layout/Header.js
+++ leadmanager/frontend/src/components/layout/Header.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { }
 
 export class Header extends Component {
     render() {
