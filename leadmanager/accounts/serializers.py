d89d512dd20d96c868d01909775519bd2d1fc652
amreshk005 <amreshk005@gmail.com>
2019-08-30 20:34:49 +0530

    login error solved

--- leadmanager/accounts/serializers.py
+++ leadmanager/accounts/serializers.py
@@ -25,3 +25,14 @@ class RegisterSerializer(serializers.ModelSerializer):
         return user
 
 # Login Serializer
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect Credenticals")
