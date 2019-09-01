20f956a6b880ccc2d644e55447c8818bbbff42d6
amreshk005 <amreshk005@gmail.com>
2019-09-01 20:44:54 +0530

    register added

--- leadmanager/frontend/src/actions/auth.js
+++ leadmanager/frontend/src/actions/auth.js
@@ -7,7 +7,9 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT_SUCCESS
+  LOGOUT_SUCCESS,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL
 } from "./types";
 
 //CHECK TOKEN AND LOAD USER
@@ -57,6 +59,35 @@ export const login = (username, password) => dispatch => {
       dispatch(returnErrors(err.response.data, err.response.status));
       dispatch({
         type: LOGIN_FAIL
+      });
+    });
+};
+
+//REGISTER USER
+
+export const register = ({ username, password, email }) => dispatch => {
+  // Headers
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  //Request Body
+  const body = JSON.stringify({ username, password, email, password });
+
+  axios
+    .post("/api/auth/register", body, config)
+    .then(res => {
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status));
+      dispatch({
+        type: REGISTER_FAIL
       });
     });
 };
